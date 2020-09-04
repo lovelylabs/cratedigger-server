5e425c34543e1c01d241d8e451daf5e04eeef4f7
Alexander Guerra <alex@lovelylabs.io>
2020-09-04 10:52:15 -0500

    Simple server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('Hello, World!')
+
+const express = require('express')
+
+const app = express()
+
+app.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`)
+})

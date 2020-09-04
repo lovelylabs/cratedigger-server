0c2f47ef07f8e098fa81ce8a0956b954a8a01ae1
Alexander Guerra <alex@lovelylabs.io>
2020-09-04 11:25:05 -0500

    Fixed server

--- index.js
+++ index.js
@@ -50,6 +50,6 @@ const app = express()
 
 server.applyMiddleware({ app })
 
-app.listen({ port: 4000 }, () =>
+app.listen({ port: process.env.PORT || 4000 }, () =>
     console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 )
